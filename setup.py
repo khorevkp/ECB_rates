bba49356c2c33c59dc735be49c0872953161fa63
khorevkp <khorevkp@gmail.com>
2022-03-05 17:48:34 +0100

    2nd version

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from setuptools import setup
 
 
 setup(
-    name='ECB_rates',                    # package name
-    version='2.0',                          # version
+    name='ecbfxrates',                    # package name
+    version='3.0',                          # version
     description='ECB Daily Rates History',   # short description
     url='https://github.com/khorevkp/ECB_rates',               # package URL
     install_requires=[],                    # list of packages this package depends
