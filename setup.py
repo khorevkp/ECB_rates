7c214e0ec59ae0a190277fe8b8b2cb59f11e1e50
khorevkp <khorevkp@gmail.com>
2022-03-06 18:26:30 +0100

    2nd version

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup
 
 
 setup(
-    name='ecbfxrates',                    # package name
+    name = 'ecbfxrates',                    # package name
     version='1.0',                          # version
     description='ECB Daily Rates History',   # short description
-    url='https://github.com/khorevkp/ECB_rates',               # package URL
-    install_requires=[],                    # list of packages this package depends
+    url='https://github.com/khorevkp/ECB_rates',    # package URL
+    install_requires=['pandas', 'requests'],    # list of packages this package depends
                                             # on.
     packages=setuptools.find_packages(exclude=['tests*']),
 )
